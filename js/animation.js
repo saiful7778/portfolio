05168fc06bbf5d95ac423b41815ebdbfaf5fb202
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-04-07 07:48:42 +0600

    add all file

---
+++ js/animation.js
@@ -0,0 +1,25 @@
+// const topBtn = document.querySelector(".top-btn");
+
+// const onObserverOptions = {
+//   threshold: 0,
+//   rootMargin: "0px 0px -250px 0px",
+// };
+
+// const allSectionOnObserver = new IntersectionObserver(function (
+//   entries,
+//   appearOnScroll
+// ) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     } else {
+//       entry.target.classList.add("appear");
+//       appearOnScroll.unobserve(entry.target);
+//     }
+//   });
+// },
+// onObserverOptions);
+
+// const allSection = document.querySelectorAll("section");
+
+// allSection.forEach((ele) => allSectionOnObserver.observe(ele));
