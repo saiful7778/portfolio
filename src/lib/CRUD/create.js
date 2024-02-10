99062000e5a235265b817bee4ba32a9df066200c
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 01:09:07 +0600

    Add data create api request

---
+++ src/lib/CRUD/create.js
@@ -0,0 +1,18 @@
+export default async function create(apiRoute, data) {
+  try {
+    const res = await fetch(apiRoute, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await res.json();
+    if (!resData.success) {
+      throw new Error(resData.message);
+    }
+    return resData;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
