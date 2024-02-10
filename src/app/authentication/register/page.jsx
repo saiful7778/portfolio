350537b2d9eba0e41eae2e42e4d13036cca7c8b2
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 01:09:27 +0600

    Add full user register page

---
+++ src/app/authentication/register/page.jsx
@@ -0,0 +1,165 @@
+"use client";
+// packages
+import { Form, Formik } from "formik";
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+// next.js packages
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+// custom hooks
+import useStateData from "@/hooks/useStateData";
+// components
+import Button from "@/components/utilities/Button";
+import Input from "@/components/utilities/Input";
+import ImageUpload from "@/components/ImageUpload";
+import Spinner from "@/components/Spinner";
+import Password from "@/components/utilities/Password";
+// api operations
+import reCaptcha from "@/lib/reCaptcha";
+import imageUpload from "@/lib/imageUpload";
+import create from "@/lib/CRUD/create";
+// others
+import { registerSchema } from "@/schemas/authentication";
+import Alert from "@/lib/config/Alert.config";
+
+const RegisterPage = () => {
+  const [spinner, setSpinner] = useState(false);
+  const recaptchaRef = useRef(null);
+  const route = useRouter();
+  const { showReCaptcha } = useStateData();
+
+  // Image data
+  const [profileImage, setProfileImage] = useState({
+    image: null,
+    name: "",
+    size: "",
+    type: "",
+    alt: "",
+  });
+  const handleProfileImage = (imageData) => {
+    setProfileImage({ ...profileImage, ...imageData });
+  };
+
+  const initialValues = {
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const handleReset = (resetForm) => {
+    return () => {
+      resetForm();
+      setSpinner(false);
+      handleProfileImage({
+        image: null,
+        name: "",
+        size: "",
+        type: "",
+        alt: "",
+      });
+    };
+  };
+
+  const submitData = async (e, { resetForm }) => {
+    setSpinner(true);
+    const reset = handleReset(resetForm);
+    if (showReCaptcha) {
+      const captcha = await reCaptcha(recaptchaRef, () => {
+        setSpinner(false);
+      });
+      if (!captcha) {
+        reset();
+        return;
+      }
+    }
+    try {
+      if (profileImage?.image) {
+        const data = await imageUpload(profileImage.image, profileImage.name);
+        const userData = {
+          name: e.fullName,
+          email: e.email,
+          password: e.password,
+          image: data?.data?.thumb?.url,
+        };
+        await createUser(userData);
+      } else {
+        const userData = {
+          name: e.fullName,
+          email: e.email,
+          password: e.password,
+          image: null,
+        };
+        await createUser(userData);
+      }
+      route.push("/");
+      reset();
+    } catch (err) {
+      console.error(err);
+      Alert.fire({
+        icon: "error",
+        text: err,
+      });
+      reset();
+    }
+  };
+
+  return (
+    <div className="rounded-md border border-gray-700 bg-slate-800 p-4 shadow-lg">
+      <h3 className="text-center text-3xl font-bold">Register</h3>
+      <ImageUpload
+        handleImageData={handleProfileImage}
+        imageData={profileImage}
+      />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={registerSchema}
+        onSubmit={submitData}
+      >
+        <Form className="mt-4 space-y-2">
+          <Input type="text" name="fullName" placeholder="Full Name" />
+          <Input type="text" name="email" placeholder="Email address" />
+          <Password placeholder="Password" name="password" />
+          <Password placeholder="Confirm password" name="confirmPassword" />
+          {showReCaptcha && (
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              sitekey={process.env.NEXT_PUBLIC_SITE_KEY}
+            />
+          )}
+          <Button
+            variant="primary"
+            className="inline-flex w-full items-center justify-center"
+            disabled={spinner}
+            type="submit"
+          >
+            {spinner ? <Spinner size={20} /> : "Register"}
+          </Button>
+        </Form>
+      </Formik>
+      <p className="mt-4 text-center text-sm text-gray-300">
+        Go back to{" "}
+        <Link href="/" className="link">
+          Home
+        </Link>
+        <Link href="/authentication/login" className="link ml-1">
+          Login
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+const createUser = async (userData) => {
+  try {
+    await create("/api/auth/register", userData);
+    Alert.fire({
+      icon: "success",
+      title: "Account is created!",
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export default RegisterPage;
