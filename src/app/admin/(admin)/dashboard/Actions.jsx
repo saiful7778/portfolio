ba6681e81b1241a4519935887b53bf442fb4dfac
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 20:41:39 +0600

    Add user management function

---
+++ src/app/admin/(admin)/dashboard/Actions.jsx
@@ -0,0 +1,256 @@
+"use client";
+// packages
+import { Form, Formik } from "formik";
+import Image from "next/image";
+// hooks
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+// components
+import { Popover } from "keep-react";
+import ImageUpload from "@/components/ImageUpload";
+import Button from "@/components/utilities/Button";
+import Input from "@/components/utilities/Input";
+import Spinner from "@/components/Spinner";
+import Select from "@/components/utilities/Select";
+import Modal from "@/components/Modal";
+// icons
+import { FaTrashCan } from "react-icons/fa6";
+import { CiEdit } from "react-icons/ci";
+import { BsThreeDotsVertical } from "react-icons/bs";
+// api op
+import imageUpload from "@/lib/imageUpload";
+import { update } from "@/lib/CRUD/update";
+import { deleteData } from "@/lib/CRUD/delete";
+// other
+import Alert from "@/lib/config/Alert.config";
+import { userSchema } from "@/schemas/user";
+
+const Actions = ({ userData }) => {
+  const { data, status } = useSession();
+  const [modal, setModal] = useState(false);
+  const [spinner, setSpinner] = useState(false);
+  const [updateImg, setUpdateImg] = useState(userData?.image ? true : false);
+  const [profileImg, setProfileImg] = useState({
+    image: null,
+    name: "",
+    size: "",
+    type: "",
+    alt: "",
+  });
+
+  const handleProfileImg = (imageData) => {
+    setProfileImg({ ...profileImg, ...imageData });
+  };
+
+  const handleDelete = async () => {
+    if (status === "authenticated" && userData.email === data?.user?.email) {
+      Alert.fire({
+        icon: "warning",
+        text: "You can't delete current logged user.",
+      });
+      return;
+    }
+    const { isConfirmed } = await Alert.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+    });
+    if (isConfirmed) {
+      Alert.fire({
+        didOpen: () => {
+          Alert.showLoading();
+        },
+      });
+      try {
+        await deleteData("/api/data/user", userData.id);
+        Alert.fire({
+          icon: "success",
+          title: "User is deleted!",
+        });
+      } catch (err) {
+        console.error(err);
+        Alert.fire({
+          icon: "error",
+          text: err,
+        });
+      }
+    }
+  };
+
+  const handleModal = () => setModal((l) => !l);
+
+  const userDataInitialValues = {
+    name: userData.name,
+    role: userData.role,
+  };
+
+  const roleOptions = [
+    { value: "user", text: "User" },
+    { value: "admin", text: "Admin" },
+  ];
+
+  const handleReset = (resetForm) => {
+    return () => {
+      resetForm();
+      setSpinner(false);
+      setModal(false);
+      handleProfileImg({
+        image: null,
+        name: "",
+        size: "",
+        type: "",
+        alt: "",
+      });
+    };
+  };
+
+  const handleUpdate = async (e, { resetForm }) => {
+    setSpinner(true);
+    const reset = handleReset(resetForm);
+    try {
+      if (!updateImg && profileImg.image) {
+        const data = await imageUpload(profileImg.image, profileImg.name);
+        await updateUser({
+          query: {
+            id: userData.id,
+            email: userData.email,
+          },
+          data: { ...e, image: { url: data?.data?.thumb?.url } },
+        });
+      } else {
+        await updateUser({
+          query: {
+            id: userData.id,
+            email: userData.email,
+          },
+          data: e,
+        });
+      }
+      reset();
+    } catch (err) {
+      console.error(err);
+      Alert.fire({
+        icon: "error",
+        text: err,
+      });
+      reset();
+    }
+  };
+
+  return (
+    <>
+      <Popover
+        showDismissIcon={false}
+        showArrow={false}
+        position="left"
+        className="border border-gray-700 !bg-gray-800 !px-2 !py-2 "
+      >
+        <Popover.Action>
+          <Button size="sm" variant="primary-outline" shape="icon-button">
+            <BsThreeDotsVertical size={15} />
+          </Button>
+        </Popover.Action>
+        <Popover.Container className="!mt-0 !block">
+          <ul>
+            <li>
+              <button
+                onClick={handleDelete}
+                className="flex w-full items-center justify-between gap-4 rounded px-2 py-1 hover:bg-gray-700"
+              >
+                <span>Delete</span>
+                <span>
+                  <FaTrashCan />
+                </span>
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={handleModal}
+                className="flex w-full items-center justify-between gap-4 rounded px-2 py-1 hover:bg-gray-700"
+              >
+                <span>Edit</span>
+                <span>
+                  <CiEdit strokeWidth={1} />
+                </span>
+              </button>
+            </li>
+          </ul>
+        </Popover.Container>
+      </Popover>
+      <Modal
+        openModal={modal}
+        closeModal={handleModal}
+        modalTitle={`Update ${userData.name}`}
+      >
+        {updateImg ? (
+          <div className="flex flex-col items-center gap-2">
+            <Image
+              src={userData.image?.url}
+              width={150}
+              height={150}
+              alt={`${userData.name} image`}
+            />
+            <Button
+              onClick={() => setUpdateImg((l) => !l)}
+              size="sm"
+              variant="cancel"
+            >
+              Remove
+            </Button>
+          </div>
+        ) : (
+          <ImageUpload
+            handleImageData={handleProfileImg}
+            imageData={profileImg}
+          />
+        )}
+        <Formik
+          initialValues={userDataInitialValues}
+          validationSchema={userSchema}
+          onSubmit={handleUpdate}
+        >
+          <Form className="mt-2">
+            <div className="flex w-full gap-2">
+              <Input
+                type="text"
+                className="flex-1"
+                placeholder="Name"
+                name="name"
+              />
+              <Select
+                name="role"
+                defaultValue="--select-user-role"
+                options={roleOptions}
+              />
+            </div>
+            <Button
+              className="mt-2"
+              disabled={spinner}
+              type="submit"
+              size="sm"
+              variant="confirm"
+            >
+              {spinner ? <Spinner size={15} /> : "Submit"}
+            </Button>
+          </Form>
+        </Formik>
+      </Modal>
+    </>
+  );
+};
+
+const updateUser = async (userData) => {
+  try {
+    await update("/api/data/user", userData);
+    Alert.fire({
+      icon: "success",
+      title: "User details is updated!",
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export default Actions;
--- src/app/admin/(admin)/dashboard/page.jsx
+++ src/app/admin/(admin)/dashboard/page.jsx
@@ -1,10 +1,91 @@
+import Avatar from "@/components/Avatar";
+import Table from "@/components/table";
+import { read } from "@/lib/CRUD/read";
+import Actions from "./Actions";
+import moment from "moment";
+
 export const metadata = {
   title: "Deshboard - admin - portfolio",
   description: "This is dashboard page of Saiful Islam portfolio website.",
 };
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+const DashboardPage = async () => {
+  const users = await read("/api/data/users");
+
+  const renderUser = users.map((user, idx) => (
+    <TableDataRow key={user.name} inputData={user} count={idx + 1} />
+  ));
+
+  return (
+    <Table>
+      <Table.caption className="bg-gray-800 p-2 font-semibold">
+        Total users: <span>{users.length}</span>
+      </Table.caption>
+      <Table.head>
+        <Table.headCell className="min-w-9">#NO</Table.headCell>
+        <Table.headCell className="min-w-80">User Details</Table.headCell>
+        <Table.headCell className="min-w-24">Role</Table.headCell>
+        <Table.headCell className="min-w-56">Time</Table.headCell>
+        <Table.headCell className="min-w-16">Actions</Table.headCell>
+      </Table.head>
+      <Table.body>{renderUser}</Table.body>
+    </Table>
+  );
+};
+
+const TableDataRow = ({ inputData, count }) => {
+  const { name, email, image, role, emailVerified, createdAt, updatedAt } =
+    inputData || "";
+  const createdTime = moment(createdAt).format("Do MMM YY, h:mm a");
+  const updatedTime = moment(updatedAt).format("Do MMM YY, h:mm a");
+
+  return (
+    <Table.row>
+      <Table.cell className="text-center font-semibold">{count}</Table.cell>
+      <Table.cell>
+        <div className="flex items-center gap-2">
+          <Avatar size="sm" photoURL={image?.url} />
+          <div>
+            <div className="font-semibold leading-tight">{name}</div>
+            <div className="text-xs text-gray-400">
+              {email}
+              <span className="ml-1 text-sky-500">
+                {emailVerified ? "verified" : "not verified"}
+              </span>
+            </div>
+          </div>
+        </div>
+      </Table.cell>
+      <Table.cell>
+        <div className="flex items-center justify-center">
+          {role === "admin" ? (
+            <span className="select-none rounded border border-green-600 bg-green-700 px-2 py-1 text-xs font-medium">
+              Admin
+            </span>
+          ) : (
+            <span className="select-none rounded border border-green-700 bg-green-900 px-2 py-1 text-xs font-medium">
+              User
+            </span>
+          )}
+        </div>
+      </Table.cell>
+      <Table.cell className="text-xs">
+        <div>
+          <span className="font-semibold text-gray-500">Create:</span>{" "}
+          {createdTime}
+        </div>
+        <div>
+          <span className="font-semibold text-gray-500">Update:</span>{" "}
+          {updatedTime}
+        </div>
+      </Table.cell>
+      <Table.cell>
+        <div className="flex items-center justify-center">
+          <Actions userData={inputData} />
+        </div>
+      </Table.cell>
+    </Table.row>
+  );
 };
 
 export default DashboardPage;
