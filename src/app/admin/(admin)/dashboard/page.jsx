46574b878bfaa6c1905c0bc8a088eedf33cf65a0
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 18:48:49 +0600

    Add metadata

--- src/app/admin/(admin)/dashboard/page.jsx
+++ src/app/admin/(admin)/dashboard/page.jsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Deshboard - admin - portfolio",
+  description: "This is dashboard page of Saiful Islam portfolio website.",
+};
+
 const DashboardPage = () => {
   return <div>DashboardPage</div>;
 };
