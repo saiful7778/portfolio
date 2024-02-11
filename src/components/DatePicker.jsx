1681ca6d4d3f4075ada5894d328e3a3e6490092d
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 21:34:05 +0600

    Add react datepicker custom styles

---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,29 @@
+"use client";
+import cn from "@/lib/cn";
+import { focus } from "@/theme";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const style = {
+  base: "border border-gray-700 select-none appearance-none rounded-md bg-gray-900 px-3 py-2 text-sm",
+  size: {
+    xs: "min-w-52",
+    sm: "min-w-64",
+    md: "min-w-80",
+  },
+};
+
+const DatePickerComp = ({ label, date, setDate, className }) => {
+  return (
+    <div>
+      <div className="mb-1 ml-2 text-xs font-medium text-gray-300">{label}</div>
+      <DatePicker
+        className={cn(style.base, style.size.md, focus.base, className)}
+        selected={date}
+        onChange={(date) => setDate(date)}
+      />
+    </div>
+  );
+};
+
+export default DatePickerComp;
