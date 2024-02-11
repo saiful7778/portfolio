bccb7dfd42f1cc565b8e62a9dea6211a59636a1c
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 21:12:27 +0600

    Add revalidate query tag

--- src/lib/CRUD/read.js
+++ src/lib/CRUD/read.js
@@ -1,9 +1,13 @@
 import getServerUrl from "../getServerUrl";
 
-export async function read(apiRoute) {
+export async function read(apiRoute, queryTags) {
   try {
     const url = getServerUrl();
-    const res = await fetch(url + apiRoute);
+    const res = await fetch(url + apiRoute, {
+      next: {
+        tags: queryTags,
+      },
+    });
     const resData = await res.json();
     if (!resData.success) {
       throw new Error(resData?.message);
