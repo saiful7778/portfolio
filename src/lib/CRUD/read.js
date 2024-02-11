f249ce578be913f1f30ab38fd80a6e189942eb88
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-11 19:36:40 +0600

    read api op

---
+++ src/lib/CRUD/read.js
@@ -0,0 +1,15 @@
+import getServerUrl from "../getServerUrl";
+
+export async function read(apiRoute) {
+  try {
+    const url = getServerUrl();
+    const res = await fetch(url + apiRoute);
+    const resData = await res.json();
+    if (!resData.success) {
+      throw new Error(resData.message);
+    }
+    return resData;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
