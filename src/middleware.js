3d9bef99c29721b375d6c990eb63ba1e83dde98b
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-15 11:47:19 +0600

    Add protected route middleware

---
+++ src/middleware.js
@@ -0,0 +1,20 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    if (
+      req.nextUrl.pathname.startsWith("/admin") &&
+      req.nextauth.token?.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/authentication/login", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token?.role === "admin",
+    },
+  },
+);
+
+export const config = { matcher: ["/admin/:path*"] };
