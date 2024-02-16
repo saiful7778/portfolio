6a1d127a66e8190747f4ff4127a8b1aaf529c388
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-17 00:00:17 +0600

    add settings form schema

---
+++ src/schemas/settings.js
@@ -0,0 +1,11 @@
+import * as Yup from "yup";
+
+export const settingsSchema = Yup.object().shape({
+  reCaptcha: Yup.string().required("Required"),
+  reCaptchaOnPage: Yup.array().when("reCaptcha", ([reCaptcha], schema) => {
+    if (reCaptcha === "custom") {
+      return schema.min(1, "Please select at least one option");
+    }
+    return schema;
+  }),
+});
