b6286653ed5605e382c9e182203711f9a863e42e
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-18 01:05:28 +0600

    Add ref input field

---
+++ src/components/utilities/InputRef.jsx
@@ -0,0 +1,35 @@
+"use client";
+import cn from "@/lib/cn";
+import { focus, input } from "@/theme";
+import { useField } from "formik";
+import { useEffect } from "react";
+
+const InputRef = ({ className, refName, customFunction, ...props }) => {
+  const [field, { touched, error }, { setValue }] = useField(props);
+  const [refField] = useField(refName);
+
+  useEffect(() => {
+    setValue(customFunction(refField.value));
+  }, [refField.value]);
+
+  return (
+    <div className={className}>
+      <input
+        className={cn(
+          input.base,
+          focus.base,
+          error && touched && input.error,
+          error && touched && focus.error,
+          className,
+        )}
+        {...field}
+        {...props}
+      />
+      {error && touched ? (
+        <p className="mt-1 text-xs text-red-500">{error}</p>
+      ) : null}
+    </div>
+  );
+};
+
+export default InputRef;
