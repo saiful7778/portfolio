6c4a0b0a213f67334fffb852c832260c75c84d46
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-20 16:34:14 +0600

    Add edge store

---
+++ src/app/api/edgestore/[...edgestore]/route.js
@@ -0,0 +1,27 @@
+import { initEdgeStore } from "@edgestore/server";
+import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
+import { z } from "zod";
+
+const es = initEdgeStore.create();
+
+const edgeStoreRouter = es.router({
+  portfolioImages: es
+    .imageBucket({
+      maxSize: 1024 * 1024 * 5,
+      accept: ["image/jpeg", "image/jpg", "image/png"],
+    })
+    .input(
+      z.object({
+        type: z.enum(["project", "blog"]),
+      }),
+    )
+    .beforeDelete(() => {
+      return true;
+    }),
+});
+
+const handler = createEdgeStoreNextHandler({
+  router: edgeStoreRouter,
+});
+
+export { handler as GET, handler as POST };
