76824c9a2e56aef366217ec4bfa0fe3ca615ee58
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-20 23:33:52 +0600

    Add custom editable slug

---
+++ src/components/EditSlug.jsx
@@ -0,0 +1,46 @@
+"use client";
+import { useField } from "formik";
+import CheckboxItem from "./utilities/CheckboxItem";
+import InputRef from "./utilities/InputRef";
+import cn from "@/lib/cn";
+
+const EditSlug = ({
+  checkboxName,
+  checkboxLabel,
+  refName,
+  inputName,
+  inputPlaceholder,
+}) => {
+  const [{ value }] = useField(checkboxName);
+  return (
+    <>
+      <CheckboxItem
+        name={checkboxName}
+        id={checkboxName}
+        label={checkboxLabel}
+      />
+      <div
+        className={cn(
+          "grid transition-all duration-300 ease-in-out",
+          value
+            ? "mt-1 grid-rows-[1fr] opacity-100"
+            : "grid-rows-[0fr] opacity-0",
+        )}
+      >
+        <InputRef
+          refName={refName}
+          className="overflow-hidden"
+          customFunction={(inputData) =>
+            inputData.toLowerCase().replace(/\s/g, "_").replace(/-/g, "")
+          }
+          type="text"
+          placeholder={inputPlaceholder}
+          name={inputName}
+          maxLength={50}
+        />
+      </div>
+    </>
+  );
+};
+
+export default EditSlug;
