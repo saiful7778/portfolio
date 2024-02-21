a6bc460e81f38d7b235abe9cbe821e4aea002d8b
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-02-21 17:51:55 +0600

    remove transition

--- src/components/PopOver.jsx
+++ src/components/PopOver.jsx
@@ -1,12 +1,11 @@
 "use client";
-import { Popover, Transition } from "@headlessui/react";
+import { Popover } from "@headlessui/react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { buttonStyle } from "./utilities/Button";
 import { FaTrashCan } from "react-icons/fa6";
 import { CiEdit } from "react-icons/ci";
 import { focus } from "@/theme";
 import cn from "@/lib/cn";
-import { Fragment } from "react";
 
 const PopOver = ({ handleDelete, handleUpdate }) => {
   return (
@@ -22,36 +21,26 @@ const PopOver = ({ handleDelete, handleUpdate }) => {
       >
         <BsThreeDotsVertical size={15} />
       </Popover.Button>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-x-1"
-        enterTo="opacity-100 translate-x-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-x-0"
-        leaveTo="opacity-0 translate-x-1"
-      >
-        <Popover.Panel className="absolute bottom-0 right-full mr-1 overflow-hidden rounded border border-gray-700 bg-gray-800">
-          <button
-            onClick={handleDelete}
-            className="flex w-full items-center justify-between gap-4 px-2 py-1.5 hover:bg-gray-700"
-          >
-            <span>Delete</span>
-            <span>
-              <FaTrashCan />
-            </span>
-          </button>
-          <button
-            onClick={handleUpdate}
-            className="flex w-full items-center justify-between gap-4 px-2 py-1.5 hover:bg-gray-700"
-          >
-            <span>Edit</span>
-            <span>
-              <CiEdit strokeWidth={1} />
-            </span>
-          </button>
-        </Popover.Panel>
-      </Transition>
+      <Popover.Panel className="absolute bottom-0 right-full mr-1 overflow-hidden rounded border border-gray-700 bg-gray-800">
+        <button
+          onClick={handleDelete}
+          className="flex w-full items-center justify-between gap-4 px-2 py-1.5 hover:bg-gray-700"
+        >
+          <span>Delete</span>
+          <span>
+            <FaTrashCan />
+          </span>
+        </button>
+        <button
+          onClick={handleUpdate}
+          className="flex w-full items-center justify-between gap-4 px-2 py-1.5 hover:bg-gray-700"
+        >
+          <span>Edit</span>
+          <span>
+            <CiEdit strokeWidth={1} />
+          </span>
+        </button>
+      </Popover.Panel>
     </Popover>
   );
 };
