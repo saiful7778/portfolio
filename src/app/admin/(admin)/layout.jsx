58e35a51b52e960b2452b26b720db7fa48ccba36
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-09 12:00:10 +0600

    add: footer

--- src/app/admin/(admin)/layout.jsx
+++ src/app/admin/(admin)/layout.jsx
@@ -1,18 +1,31 @@
 "use client";
 import cn from "@/lib/utils/cn";
 import useStateData from "@/hooks/useStateData";
+import Link from "next/link";
 
 const ClientAdminLayout = ({ children }) => {
   const { sidebar } = useStateData();
   return (
-    <main
+    <div
       className={cn(
-        "mt-10 p-2 duration-300",
+        "mt-10 duration-300",
         sidebar ? "md:ml-36" : "md:ml-[43px]",
       )}
     >
-      {children}
-    </main>
+      <main className="mb-10 p-2">{children}</main>
+      <footer className="border-t border-gray-700 bg-gray-800 p-6 text-center text-gray-400">
+        <p>
+          © 2023 Saiful Islam. All rights reserved by{" "}
+          <Link
+            className="link"
+            href="https://github.com/saiful7778"
+            target="_blank"
+          >
+            Saiful Islam
+          </Link>
+        </p>
+      </footer>
+    </div>
   );
 };
 
