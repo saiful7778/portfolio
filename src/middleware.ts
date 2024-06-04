de037c18877ea207063675ef7442acc94d7bbd36
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-04 15:24:56 +0600

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(function middleware(req) {
+  const isAuth = !!req.nextauth;
+
+  if (!isAuth) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+  return null;
+});
+
+export const config = { matcher: ["/admin/:path*"] };
