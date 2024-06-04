7fb209d325f32c82d181a25e5af87b56f28d52ad
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-04 18:07:32 +0600

    add auth user redirect

--- src/app/(frontend)/(auth)/layout.tsx
+++ src/app/(frontend)/(auth)/layout.tsx
@@ -1,6 +1,17 @@
+import { defaultLoginRedirect } from "@/lib/routes";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-export default function AuthLayout({ children }: { children: ReactNode }) {
+export default async function AuthLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession();
+  if (!!session?.user) {
+    redirect(defaultLoginRedirect);
+  }
   return (
     <div className="mx-auto my-16 w-full max-w-sm rounded-md border bg-card p-4 text-card-foreground shadow-sm">
       {children}
