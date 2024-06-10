24c8f4beb757659f8bbf8ce0b9ae65d1f1aaa6a2
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-11 01:06:27 +0600

    add update user server action

---
+++ src/lib/actions/user/updateUser.ts
@@ -0,0 +1,29 @@
+"use server";
+import db from "@/lib/db";
+import { updateUserSchema } from "@/lib/schemas/auth";
+import { z } from "zod";
+
+export default async function updateUser(e: z.infer<typeof updateUserSchema>) {
+  try {
+    const isValid = updateUserSchema.safeParse(e);
+
+    if (!isValid.success) {
+      throw new Error("Invalid input data");
+    }
+
+    const user = await db.user.update({
+      where: {
+        email: isValid.data.email,
+      },
+      data: { ...isValid.data },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    }
+  }
+}
