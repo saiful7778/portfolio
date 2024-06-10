9b2000c4f086632d1a9912f0a0745c241685902e
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-11 00:56:40 +0600

    add gradiant blob

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,15 @@
 "use client";
+import {
+  DestructiveColorBlob,
+  PrimaryColorBlob,
+} from "@/components/GradiantBlob";
 import Button from "@/components/ui/button";
 
 const Error = ({ reset }: { reset: () => void }) => {
   return (
-    <section className="flex h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden">
+    <section className="relative flex h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden">
+      <PrimaryColorBlob className="-left-96 top-0" />
+      <DestructiveColorBlob className="-right-96 top-28" />
       <h2 className="text-2xl font-bold">
         Something went <span className="text-red-600">wrong!</span>
       </h2>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,15 @@
+import {
+  DestructiveColorBlob,
+  PrimaryColorBlob,
+} from "@/components/GradiantBlob";
 import Button from "@/components/ui/button";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center gap-2">
+    <main className="relative flex h-screen w-full flex-col items-center justify-center gap-2">
+      <PrimaryColorBlob className="-left-96 top-0" />
+      <DestructiveColorBlob className="-right-96 top-28" />
       <h2 className="text-9xl font-bold">404!</h2>
       <code>Not Found!</code>
       <p>Could not find requested resource</p>
