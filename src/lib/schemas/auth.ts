a38d35e4aa650f2a53e582b67c10b147251da3a0
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-11 01:06:39 +0600

    add update user schema

--- src/lib/schemas/auth.ts
+++ src/lib/schemas/auth.ts
@@ -1,11 +1,11 @@
 import * as z from "zod";
 
-export const LoginSchema = z.object({
-  email: z.string().email({
+export const loginSchema = z.object({
+  email: z.string({ required_error: "Email is required" }).email({
     message: "Email is required",
   }),
   password: z
-    .string()
+    .string({ required_error: "Password is required" })
     .min(1, {
       message: "Password is required",
     })
@@ -13,4 +13,15 @@ export const LoginSchema = z.object({
       message:
         "Password must required: Capital letter and a Special character.",
     }),
+});
+
+export const updateUserSchema = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(1, "Name is required"),
+  email: z.string({ required_error: "Email is required" }).email({
+    message: "Email is required",
+  }),
+  role: z.enum(["user", "admin"], { required_error: "User role is required" }),
+  access: z.boolean({ required_error: "User access is required" }),
 });
