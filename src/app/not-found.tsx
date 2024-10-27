42dad79c17c39efed9512acd3477db037536ee48
saiful7778 <saiful.islam.rafi.88@gmail.com>
2024-10-27 15:56:16 +0600

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import {
+  DestructiveColorBlob,
+  PrimaryColorBlob,
+} from "@/components/GradientBlob";
+import Button from "@/components/ui/button";
+import Link from "next/link";
+
+const NotFound: React.FC = () => {
+  return (
+    <main className="relative flex h-screen w-full flex-col items-center justify-center gap-6">
+      <PrimaryColorBlob className="-left-96 top-0" />
+      <DestructiveColorBlob className="-right-96 top-28" />
+      <h2 className="text-9xl font-bold">404!</h2>
+      <code>Not Found!</code>
+      <p>Could not find requested resource</p>
+      <Button variant="destructive" asChild>
+        <Link href="/">Return Home</Link>
+      </Button>
+    </main>
+  );
+};
+
+export default NotFound;
