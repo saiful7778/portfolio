4ba203eaec7d53b12e64845ef62b4e74d17182bf
saiful7778 <saiful.islam.rafi.88@gmail.com>
2024-10-27 16:46:00 +0600

    add metadata

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -1,4 +1,11 @@
 import SectionElement from "@/components/SectionElement";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Projects - Saiful Islam - Portfolio",
+  description:
+    "This is project showcase page of Saiful Islam personal portfolio website.",
+};
 
 const Project: React.FC = () => {
   return (
